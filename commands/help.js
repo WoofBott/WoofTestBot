8aea337e153bf5f610487d8391d98f6717d9de71
RB <80545089+WoofBott@users.noreply.github.com>
2021-06-29 21:19:38 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ module.exports = {
  callback(client,message){
    let embed = new Discord.MessageEmbed()
     .setTitle("Commands")
-    .setDescription("Bot prefix: `w!` \n\n**Owner** \n`WIP` \n**Moderation** \n`kick` \n**Utility** \n`purge`,`slowmode` \n**Posting** \n`report` \n**Roles** \n`WIP` \n**Fun** \n`nameme`,`kill`,`gayrate` \n**Information** \n`embed`,`ping`,`dm`,`Rreact`,`botinfo`,membercount")
+    .setDescription("Bot prefix: `w!` \n\n**Owner** \n`WIP` \n**Moderation** \n`kick` \n**Utility** \n`purge`,`slowmode` \n**Posting** \n`report` \n**Roles** \n`WIP` \n**Fun** \n`nameme`,`kill`,`gayrate` \n**Information** \n`embed`,`ping`,`dm`,`Rreact`,`botinfo`,`membercount`")
     .setColor("#e9c172")
     .setFooter("WoofTest - Version 1.0");
  message.channel.send(embed);
